18a7e2cd6e8f8fd1f9fea8e53943ccbcc50781f6
Mojtaba Michael <mojimich2015@gmail.com>
2024-12-02 11:21:25 -0800

    Added structure for books apis

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func (app *application) handleHealthCheck(w http.ResponseWriter, r *http.Request) {
@@ -13,4 +14,53 @@ func (app *application) handleHealthCheck(w http.ResponseWriter, r *http.Request
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", "dev")
 	fmt.Fprintf(w, "version: %s\n", "1.0.0")
+}
+
+func (app *application) handleGetCreateBooks(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		fmt.Fprintln(w, "Display a list of the books")
+	}
+	if r.Method == http.MethodPost {
+		fmt.Fprintln(w, "Added a new book to the reading list")
+	}
+}
+
+func (app *application) handleGetUpdateBooks(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		app.getBook(w, r)
+	case http.MethodPut:
+		app.updateBook(w, r)
+	case http.MethodDelete:
+		app.deleteBook(w, r)
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
+
+func (app *application) getBook(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("v1/books/"):]
+	idInt, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+	}
+	fmt.Fprintf(w, "Display the details of book with ID: %d", idInt)
+}
+
+func (app *application) updateBook(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("v1/books/"):]
+	idInt, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+	}
+	fmt.Fprintf(w, "Update the details of book with ID: %d", idInt)
+}
+
+func (app *application) deleteBook(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("v1/books/"):]
+	idInt, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+	}
+	fmt.Fprintf(w, "Delete the details of book with ID: %d", idInt)
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,5 +5,7 @@ import "net/http"
 func (app *application) route() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/v1/healthcheck", app.handleHealthCheck)
+	mux.HandleFunc("/v1/books", app.handleGetCreateBooks)
+	mux.HandleFunc("/v1/books/", app.handleGetUpdateBooks)
 	return mux
 }
