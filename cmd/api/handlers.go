5c37c7f7bb6c66812245d72b3fb1d2ba52df9d42
Mojtaba Michael <mojimich2015@gmail.com>
2024-12-03 06:18:01 -0800

    Added readJSON helper

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -60,7 +60,20 @@ func (app *application) handleGetCreateBooks(w http.ResponseWriter, r *http.Requ
 		}
 	}
 	if r.Method == http.MethodPost {
-		fmt.Fprintln(w, "Added a new book to the reading list")
+		var input struct {
+			Title     string   `json:"title"`
+			Published int      `json:"published"`
+			Pages     int      `json:"pages"`
+			Genres    []string `json:"rating"`
+			Rating    float64  `json:"rating"`
+		}
+		err := app.readJSON(w, r, &input)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "%v\n", input)
+
 	}
 }
 
@@ -107,7 +120,44 @@ func (app *application) updateBook(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
-	fmt.Fprintf(w, "Update the details of book with ID: %d", idInt)
+	var input struct {
+		Title     *string  `json:"title"`
+		Published *int     `json:"published"`
+		Pages     *int     `json:"pages"`
+		Genres    []string `json:"genres"`
+		Rating    *float32 `json:"rating"`
+	}
+	book := data.Book{
+		ID:        idInt,
+		CreatedAt: time.Now(),
+		Title:     "Echoes in the Darkness",
+		Published: 2019,
+		Pages:     300,
+		Genres:    []string{"Fiction", "Thriller"},
+		Rating:    4.5,
+		Version:   1,
+	}
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	if input.Title != nil {
+		book.Title = *input.Title
+	}
+	if input.Published != nil {
+		book.Published = *input.Published
+	}
+	if input.Pages != nil {
+		book.Pages = *input.Pages
+	}
+	if len(input.Genres) > 0 {
+		book.Genres = input.Genres
+	}
+	if input.Rating != nil {
+		book.Rating = *input.Rating
+	}
+	fmt.Fprintf(w, "%v\n", book)
 }
 
 func (app *application) deleteBook(w http.ResponseWriter, r *http.Request) {
--- cmd/api/helper.go
+++ cmd/api/helper.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
 )
+
 type envelope map[string]any
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope) error {
@@ -15,5 +18,20 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	if err := dec.Decode(dst); err != nil {
+		return err
+	}
+	err := dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON object")
+	}
 	return nil
 }
