6f6803d62b804940f81de09413d6c946ecdec2c0
Mojtaba Michael <mojimich2015@gmail.com>
2024-12-03 05:52:34 -0800

    Added helpers and added book structure

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
+
+	"github.com/mjmichael73/go-books-api/internal/data"
 )
 
 func (app *application) handleHealthCheck(w http.ResponseWriter, r *http.Request) {
@@ -11,14 +15,49 @@ func (app *application) handleHealthCheck(w http.ResponseWriter, r *http.Request
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", "dev")
-	fmt.Fprintf(w, "version: %s\n", "1.0.0")
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	js, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	js = append(js, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 func (app *application) handleGetCreateBooks(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		fmt.Fprintln(w, "Display a list of the books")
+		books := []data.Book{
+			{
+				ID:        1,
+				CreatedAt: time.Now(),
+				Title:     "The Darkening of Tristram",
+				Published: 1998,
+				Pages:     300,
+				Genres:    []string{"Fiction", "Thriller"},
+				Rating:    4.5,
+				Version:   1,
+			},
+			{
+				ID:        1,
+				CreatedAt: time.Now(),
+				Title:     "The Legacy of Deckard Cain",
+				Published: 1998,
+				Pages:     300,
+				Genres:    []string{"Fiction", "Adventure"},
+				Rating:    4.5,
+				Version:   1,
+			},
+		}
+		if err := app.writeJSON(w, http.StatusOK, books); err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
 	}
 	if r.Method == http.MethodPost {
 		fmt.Fprintln(w, "Added a new book to the reading list")
@@ -39,13 +78,26 @@ func (app *application) handleGetUpdateBooks(w http.ResponseWriter, r *http.Requ
 }
 
 func (app *application) getBook(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Path[len("v1/books/"):]
+	id := r.URL.Path[len("/v1/books/"):]
 	idInt, err := strconv.ParseInt(id, 10, 64)
 	if err != nil {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
-	fmt.Fprintf(w, "Display the details of book with ID: %d", idInt)
+	book := data.Book{
+		ID:        idInt,
+		CreatedAt: time.Now(),
+		Title:     "Echoes in the Darkness",
+		Published: 2019,
+		Pages:     300,
+		Genres:    []string{"Fixtion", "Thriller"},
+		Rating:    4.5,
+		Version:   1,
+	}
+	if err := app.writeJSON(w, http.StatusOK, book); err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
 
 func (app *application) updateBook(w http.ResponseWriter, r *http.Request) {
